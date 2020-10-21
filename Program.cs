d9d7f2c1424fb2bd1f786cd4f7d0ada220458a8a
Flávio José Formis <flavio.formis@pca.com.br>
2020-10-21 16:33:50 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Yield
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (int i in Power(2, 8))
+            {
+                Console.Write("{0} ", i);
+            }
+
+            var theGalaxies = new Galaxies();
+            foreach (Galaxy theGalaxy in theGalaxies.NextGalaxy)
+            {
+                Console.WriteLine(theGalaxy.Name + " " + theGalaxy.MegaLightYears.ToString());
+            }
+        }
+
+        public static IEnumerable<int> Power(int number, int exponent)
+        {
+            int result = 1;
+            for (int i = 0; i < exponent; i++)
+            {
+                result = result * number;
+                yield return result;
+            }
+        }
+
+        public class Galaxies
+        {
+
+            public System.Collections.Generic.IEnumerable<Galaxy> NextGalaxy
+            {
+                get
+                {
+                    yield return new Galaxy { Name = "Tadpole", MegaLightYears = 400 };
+                    yield return new Galaxy { Name = "Pinwheel", MegaLightYears = 25 };
+                    yield return new Galaxy { Name = "Milky Way", MegaLightYears = 0 };
+                    yield return new Galaxy { Name = "Andromeda", MegaLightYears = 3 };
+                }
+            }
+        }
+
+        public class Galaxy
+        {
+            public String Name { get; set; }
+            public int MegaLightYears { get; set; }
+        }
+    }
+}
